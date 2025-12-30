3cb6d4989f28e9fe7fb0d0fba682621156263690
SatyamGupta78 <compareself@gmail.com>
2025-12-30 16:16:07 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/analogy-das/",
+  base: "/analogy--das/",
   server: {
     host: "::",
     port: 8080,
